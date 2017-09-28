254e9721908b7efeb3c5b68a602d384630bb33d7
Severiano Badajoz <sevebadajoz@gmail.com>
2017-09-27 22:59:43 -0700

    Initial commit

---
+++ app/src/main/java/edu/orangecoastcollege/a273/sbadajozcs/inandout/OrderActivity.java
@@ -0,0 +1,13 @@
+package edu.orangecoastcollege.a273.sbadajozcs.inandout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
