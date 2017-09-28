46e813f358e2941b314eb283b93293e85ff93b6d
Severiano Badajoz <sevebadajoz@gmail.com>
2017-09-27 23:28:50 -0700

    Finish onClick method for button

--- app/src/main/java/edu/orangecoastcollege/a273/sbadajozcs/inandout/OrderActivity.java
+++ app/src/main/java/edu/orangecoastcollege/a273/sbadajozcs/inandout/OrderActivity.java
@@ -1,7 +1,10 @@
 package edu.orangecoastcollege.a273.sbadajozcs.inandout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class OrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+    }
+
+    protected void createOrder(View view) {
+        Intent intent = new Intent(this, SummaryActivity.class);
+        // If you squint real hard you can pretend its readable code
+        Order order = new Order(Integer.valueOf(((TextView)findViewById(R.id.doubledoubleTextView)).getText().toString()),
+                Integer.valueOf(((TextView)findViewById(R.id.cheeseburgerTextView)).getText().toString()),
+                Integer.valueOf(((TextView)findViewById(R.id.frenchFriesTextView)).getText().toString()),
+                Integer.valueOf(((TextView)findViewById(R.id.shakesTextView)).getText().toString()),
+                Integer.valueOf(((TextView)findViewById(R.id.smallDrinkTextView)).getText().toString()),
+                Integer.valueOf(((TextView)findViewById(R.id.mediumDrinkTextView)).getText().toString()),
+                Integer.valueOf(((TextView)findViewById(R.id.largeDrinkTextView)).getText().toString()));
+
+        intent.putExtra("Num", order.numItems());
+        intent.putExtra("Subtotal", order.getSubtotal());
+        intent.putExtra("Tax", order.getTax());
+        intent.putExtra("Total", order.getTotal());
+
+        startActivity(intent);
     }
 }
