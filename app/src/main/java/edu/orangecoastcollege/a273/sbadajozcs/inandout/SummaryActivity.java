54b79bd44f8a6d248645c578d4570224afd7943f
Severiano Badajoz <sevebadajoz@gmail.com>
2017-09-27 23:29:00 -0700

    Create SummaryActivity.java

---
+++ app/src/main/java/edu/orangecoastcollege/a273/sbadajozcs/inandout/SummaryActivity.java
@@ -0,0 +1,13 @@
+package edu.orangecoastcollege.a273.sbadajozcs.inandout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SummaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+    }
+}
