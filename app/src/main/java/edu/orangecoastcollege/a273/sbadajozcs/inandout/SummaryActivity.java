44e9c6bbda44d512cc190255fcd07e624bf7e3c5
Severiano Badajoz <sevebadajoz@gmail.com>
2017-09-27 23:39:31 -0700

    Create UI

--- app/src/main/java/edu/orangecoastcollege/a273/sbadajozcs/inandout/SummaryActivity.java
+++ app/src/main/java/edu/orangecoastcollege/a273/sbadajozcs/inandout/SummaryActivity.java
@@ -1,5 +1,6 @@
 package edu.orangecoastcollege.a273.sbadajozcs.inandout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class SummaryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
+
+        Intent intent = getIntent();
+
     }
 }
